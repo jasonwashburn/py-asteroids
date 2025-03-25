ee52688f1e79a742a3ec159cb0cb8116da240b38
Jason Washburn <jason.washburn@gmail.com>
2025-03-25 17:19:12 -0500

    feat: Add player forward and reverse movement

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -20,8 +24,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(pygame.Color("black"))
-        player.draw(screen)
-        player.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        updateable.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
